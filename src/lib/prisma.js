ca934c2e2d9c85955d01ca45b88dbdd3efa961d8
gptjd0204 <gptjd0204@naver.com>
2026-04-13 10:10:53 +0900

    init: prettier 규칙 설정

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-  log: ["query", "error", "warn"], // 쿼리 로깅
+  log: ['query', 'error', 'warn'], // 쿼리 로깅
 });
 
 export default prisma;
