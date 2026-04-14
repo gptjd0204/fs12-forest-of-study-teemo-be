a62b8cc0fbb64e160d01e41b3eb6555a13e1882e
gptjd0204 <gptjd0204@naver.com>
2026-04-14 15:24:59 +0900

    feat: CORS 추가

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
