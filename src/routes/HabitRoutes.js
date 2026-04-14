a4bedabff3a8273a7a02b36b7d63eae55cf3e080
hogu-giriboy <choo6078@kakao.com>
2026-04-14 21:18:41 +0900

    feat: 습관 생성 API 구현

--- src/routes/HabitRoutes.js
+++ src/routes/HabitRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getTodayHabits } from '../controllers/HabitController.js';
+import { getTodayHabits, createHabit } from '../controllers/HabitController.js';
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/today', getTodayHabits);
+router.post('/', createHabit);
 
 export default router;
