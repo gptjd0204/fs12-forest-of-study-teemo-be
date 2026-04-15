4ec3bf5bef7bfa3bd9d55f46a0f0344b3cdf01ee
9g-g9 <nayun12375@gmail.com>
2026-04-15 18:17:08 +0900

    feat: 단일 스터디 조회 생성

--- src/routes/studyRoutes.js
+++ src/routes/studyRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getStudies, createStudy } from '../controllers/studyController.js';
+import {
+  getStudies,
+  createStudy,
+  getStudy,
+} from '../controllers/studyController.js';
 
 const router = express.Router();
 
 router.get('/', getStudies);
-
+router.get('/:studyId', getStudy);
 router.post('/', createStudy);
 
 export default router;
